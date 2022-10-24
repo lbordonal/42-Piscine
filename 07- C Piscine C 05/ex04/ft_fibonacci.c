4fbaf1c61db58ceca478aecd00f8e0a0afc7ff58
lbordonal <lbordonal@gmail.com>
2022-10-24 13:57:44 +0100

    C05 - update

--- 07- C Piscine C 05/ex04/ft_fibonacci.c
+++ 07- C Piscine C 05/ex04/ft_fibonacci.c
@@ -22,3 +22,11 @@ int	ft_fibonacci(int index)
 	}
 	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
 }
+
+/*#include <stdio.h>
+
+int	main(void)
+{
+	printf("%d", ft_fibonacci(5));
+	return (0);
+}*/
